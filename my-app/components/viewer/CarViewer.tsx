eaba8d6e2a8790500eed46ecd13d7f563a8a3545
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-12-17 21:16:46 +0100

    Create viewer/CarViewer.tsx

---
+++ my-app/components/viewer/CarViewer.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import CarModel from "./CarModel";
+import Lights from "./Lights";
+
+export default function CarViewer() {
+  return (
+    <Canvas
+      camera={{ position: [3, 1.5, 5], fov: 45 }}
+      className="bg-neutral-950"
+    >
+      <Lights />
+      <CarModel />
+
+      <OrbitControls
+        enablePan={false}
+        minDistance={3}
+        maxDistance={8}
+      />
+    </Canvas>
+  );
+}

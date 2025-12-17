a0f1272b1a50fdeff1b0107eee5a054da9c67341
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-12-17 21:27:21 +0100

    Update layout.tsx

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,14 +1,15 @@
 import "./globals.css";
 import { CarConfigProvider } from "@/context/CarConfigContext";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata = {
+  title: "Primyst Drive",
+  description: "3D Car Selling Experience",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-neutral-950 text-white">
         <CarConfigProvider>{children}</CarConfigProvider>
       </body>
     </html>

d7de55efd20a7450827dbf0b67a7e3875f00fc04
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-12-17 21:19:30 +0100

    Create lib/colors.ts

---
+++ my-app/lib/colors.ts
@@ -0,0 +1,12 @@
+export type CarColor = {
+  name: string;
+  hex: string;
+};
+
+export const CAR_COLORS: CarColor[] = [
+  { name: "Polar White", hex: "#f5f5f5" },
+  { name: "Obsidian Black", hex: "#0b0b0b" },
+  { name: "Iridium Silver", hex: "#bfc3c6" },
+  { name: "Graphite Grey", hex: "#3a3a3a" },
+  { name: "Deep Blue", hex: "#0f1c2e" },
+];
